fc60b86a5dd44f39bb62968abcb86636736dcdec
inov8 <blackisnewyellow@gmail.com>
2022-11-18 10:48:22 +0300

    v 0.1

--- main.py
+++ main.py
@@ -1,3 +1,69 @@
-from tvizbase.api import Api
-from operations import *
 
+import datetime
+from time import sleep
+import operations as ops
+from tvizbase.api import Api
+
+
+api = Api()
+
+awards_in_interval = dict.fromkeys(ops.cohorts, 0) 
+count_awards_in_interval = dict.fromkeys(ops.cohorts, 0)
+# ops.check_file_exist('receive_awards_by_hour.csv')
+# ops.check_file_exist('count_awards_by_hour.csv')
+
+#Узнаем дату, время и номер блока последней записи в файл. При отсутствии данных берем час создания первого блока
+last_receive_award = ops.get_last_date_and_blocknumber_in_file('receive_awards_by_hour.csv')
+last_receive_award_date = last_receive_award['Последняя сохраненная дата']
+last_receive_award_block = last_receive_award['Последний сохраненный блок']
+
+# Задаем длительность блока и интервал сканирования.
+block_time_size = datetime.timedelta(seconds=3)
+scan_interval = datetime.timedelta(minutes=60)
+
+#Определяем номер блока, с которого нужно начать сканирование.
+scan_start_block = last_receive_award_block+1
+
+#Определяем время, с которого нужно начать таймер.
+scan_start_at = last_receive_award_date + block_time_size
+
+# Парсим.
+while True:
+    # Проверяем, прошел ли интервал сканирования с времени последнего сканирования
+    if (datetime.datetime.now() - last_receive_award_date) >= scan_interval:
+            
+        block = api.get_ops_in_block(scan_start_block)
+        block_time = datetime.datetime.fromisoformat(block[0]['timestamp'])
+        for transaction in block:
+            if transaction['op'][0] in ('receive_award', 'benefactor_award', 'witness_reward'):
+                award_in_tx = ops.parse_tx(transaction)
+                cohort_in_tx = (award_in_tx[0])
+
+                awards_in_interval[cohort_in_tx] += award_in_tx[1]
+                count_awards_in_interval[cohort_in_tx] += 1
+            
+        # Проверяем, не является ли блок последним в интервале таймера сканирования
+        if block_time - scan_start_at + block_time_size >= scan_interval:
+            awards_in_interval_list = []
+            count_awards_in_interval_list = []
+            # print('Добавляем данные в файлы')
+            # Сбрасываем в 0 агрегированные данные по когортам
+            for item in ops.cohorts[1:-1]:
+                awards_in_interval_list.append(str(awards_in_interval[item]))
+                count_awards_in_interval_list.append(str(count_awards_in_interval[item]))
+                awards_in_interval[item] = 0
+                count_awards_in_interval[item] = 0
+
+            with open('receive_awards_by_hour.csv', 'a', encoding='utf-8') as f:
+                f.write(str(block_time) + ',' + ','.join(awards_in_interval_list) + ',' + str(scan_start_block) + '\n')
+
+            with open('count_receive_awards_by_hour.csv', 'a', encoding='utf-8') as f:
+                f.write(str(block_time) + ',' + ','.join(count_awards_in_interval_list) + ',' + str(scan_start_block) + '\n')
+
+            print(datetime.datetime.now(),'Файлы обновлены. Добавлены данные к дате', block_time)
+            scan_start_at = block_time + block_time_size
+        
+        scan_start_block += 1
+    else:
+        sleep(scan_interval.seconds)
+        print(datetime.datetime.now(), 'Ожидание новых данных...')
--- operations.py
+++ operations.py
@@ -1,155 +1,86 @@
-import json
-import time
-import os
-import json
-import csv
 import re
-import numpy as np
-import pandas as pd
-from datetime import datetime, timedelta
-from threading import Thread
-import params as p
-from tqdm import tqdm
-from tvizbase.api import Api
+from datetime import datetime
 
-def collect_ops_from_block(block_number): #Запись в op_string транзакций из блока block_number
-    tx = p.api.get_ops_in_block(block_number)
-    for operation in tx:
-        p.op_string.append(json.dumps(operation)+'\n')
+# Столбцы файлов данных. Многие функции ссылаются на порядок столбцов в нём
+header = 'Дата,Бенефициарские,Другое,Игровой бот dice.id,Каналы телеграм,Награды делегатам,Награды за посты в Readdle.me,Награды через Визонатор,Прямые награды,Самоапы,Самоапы viz-social-bot,Чаты телеграм,Чаты телеграм (сплит),Эирдропы в чатах телеграм,Номер последнего блока'
 
-def collect_ops_in_range(first_block, last_block): #Запись в op_string транзакций из блоков в диапазоне.
-    for block in range(first_block, last_block):
-        tx = p.api.get_ops_in_block(block)
-        time.sleep(0.1)
-        for operation in tx:
-            p.op_string.append(json.dumps(operation)+'\n')
+#Получение типа транзакции и размера награды в виде {Тип_награды : Размер_в_SHARES}
+def parse_tx(transaction):
+    tx_timestamp = datetime.fromisoformat(transaction['timestamp'])
+    tx_shares = float(transaction['op'][1]['shares'][:-7])
+    if transaction['op'][0] == 'receive_award':
+        transaction_memo = transaction['op'][1]['memo']
+        if re.match(r'telegram:\d', transaction_memo) != None:
+            tx_type = 'Чаты телеграм'
+        elif re.match(r'channel:@', transaction_memo) != None:     
+            tx_type = 'Каналы телеграм'
+        elif re.match(r'split:', transaction_memo) !=None:
+            tx_type = 'Чаты телеграм (сплит)'
+        elif re.match(r'chat:', transaction_memo) !=None:
+            tx_type = 'Эирдропы в чатах телеграм'
+        elif re.match(r'viz://@', transaction_memo) !=None:
+            tx_type = 'Награды за посты в Readdle.me'
+        elif transaction_memo in ['', 'award']:
+            if transaction['op'][1]['initiator'] == transaction['op'][1]['receiver'] == 'viz-social-bot':
+                tx_type = 'Самоапы viz-social-bot'
+            elif transaction['op'][1]['initiator'] == transaction['op'][1]['receiver']:
+                tx_type = 'Самоапы'
+            else:
+                tx_type = 'Прямые награды'
+        elif re.match(r'Награда за пост: https://golos.io/@', transaction_memo) !=None:
+            tx_type = 'Награды за посты в GOLOS.IO'
+        elif transaction['op'][1]['receiver'] == 'social':
+            tx_type = 'Награды через Визонатор'
+        elif transaction['op'][1]['initiator'] == 'dice.id':
+            tx_type = 'Игровой бот dice.id'
+        else:
+            tx_type = ('Другое')
+    if transaction['op'][0] == 'benefactor_award':
+        if transaction['op'][1]['initiator'] == 'dice.id':
+            tx_type = 'Игровой бот dice.id'
+        else:
+            tx_type = 'Бенефициарские'
+    if transaction['op'][0] == 'witness_reward':
+            tx_type = 'Награды делегатам'
+    return[tx_type, tx_shares]
 
-def blocknum(block): #Получение номера блока.
-    block=json.loads(block)
-    return block['block']
+# Проверка наличия файла и создание его, в случае необходимости
+def check_file_exist(filename):
+    try:
+        f = open(filename)
+        f.close()
+    except FileNotFoundError:
+        f = open(filename, 'w+', encoding='utf-8')
+        f.write(header)
+        f.close()
 
-def parser(): #Парсер транзакций, начиная с последнего записанного в файлы блока. Сохраняет все транзакции в файлы.
-    for item in tqdm(range(p.last_block_in_files, p.last_block_in_chain, p.step)):
-        th = 0
-        for x in range(item, item+p.step, p.r):
-            exec("thread{} = Thread(target=collect_ops_in_range, args=(x, x+{},))".format(th,p.r))
-            th+=1
-        for th in range(p.th_num):
-            exec("thread{}.start()".format(th))
-        for th in range(p.th_num):
-            exec("thread{}.join()".format(th))
-        print(len(p.op_string), 'transactions,', x, 'blocks readed')
-        if len(p.op_string) >= p.str_len:
-            p.op_string.sort(key=blocknum)
-            with open('tx_raw/tx' + str(p.f_count) + '.txt', 'w') as fp:
-                fp.writelines(p.op_string)
-            print('В файл tx'+str(p.f_count)+'.txt записано', len(p.op_string), 'транзакций')
-            p.f_count+=1
-            p.op_string=[]
-    p.op_string.sort(key=blocknum)
-    with open('tx_raw/tx' + str(p.f_count) + '.txt', 'w') as fp:
-        fp.writelines(p.op_string)
-    print('В файл tx'+str(p.f_count)+'.txt записано', len(p.op_string), 'транзакций')
-    p.op_string=[]    
+# Проверка последней сохраненной даты и номера блока. При отсутствии данных оба принимают значение 0
+def get_last_date_and_blocknumber_in_file(filename):
+    f = open(filename, 'r', encoding='utf-8')
+    line = f.readlines()[-1]
+    try:
+        last_block_in_data = int(line.split(',')[-1])
+        last_date_in_data = datetime.strptime(line.split(',')[0], '%Y-%m-%d %H:%M:%S')
+    except:
+        last_block_in_data = 0
+        last_date_in_data = datetime.strptime('2018-09-29 10:00:00', '%Y-%m-%d %H:%M:%S')
+    return{'Последняя сохраненная дата':last_date_in_data, 'Последний сохраненный блок':last_block_in_data}
 
-def create_csv():
-    f_count = len(os.listdir('tx_raw'))
-    with open('tx_raw/tx' + str(f_count) +'.txt', 'r') as fp:
-        f = fp.readlines()
-    last_block_timestamp = datetime.fromisoformat(json.loads(f[(len(f)-1)])['timestamp'])
-
-    print('Чтение транзакций из сохраненных файлов...')
-    for f_number in tqdm(range(f_count, 0, -1)):
-        with open('tx_raw/tx' + str(f_number) +'.txt', 'r') as fp:
-            f = fp.readlines()
-        for tx in reversed(f):
-            tx_timestamp = datetime.fromisoformat(json.loads(tx)['timestamp'])
-            if tx_timestamp.year == p.scan_year:
-                interval_in_secs = (last_block_timestamp-tx_timestamp).seconds
-                interval_in_days = (last_block_timestamp-tx_timestamp).days
-                interval = interval_in_days*24*60*60 + interval_in_secs
-                operation = json.loads(tx)
-                if operation['op'][0] == 'receive_award':
-                    p.block_num.append(operation['block'])
-                    p.operation_type.append(operation['op'][0])
-                    p.shares.append(float(operation['op'][1]['shares'][:-7]))
-                    p.op_in_trx.append(operation['op_in_trx'])
-                    p.timestamp.append((datetime.fromisoformat(operation['timestamp']))+timedelta(hours=1))
-                    p.trx_id.append(operation['trx_id'])
-                    p.trx_in_block.append(operation['trx_in_block'])
-                    p.virtual_op.append(operation['virtual_op'])
-                    p.initiator.append(operation['op'][1]['initiator'])
-                    p.receiver.append(operation['op'][1]['receiver'])
-                    p.custom_sequence.append(operation['op'][1]['custom_sequence'])
-                    original_memo = operation['op'][1]['memo']
-                    if re.match(r'telegram:\d', original_memo) !=None:
-                        p.memo.append('Чаты телеграм')
-                    elif re.match(r'channel:@', original_memo) != None:     
-                        p.memo.append('Каналы телеграм')
-                    elif re.match(r'split:', original_memo) !=None:
-                        p.memo.append('Чаты телеграм (сплит)')
-                    elif re.match(r'chat:', original_memo) !=None:
-                        p.memo.append('Эирдропы в чатах телеграм')
-                    elif re.match(r'viz://@', original_memo) !=None:
-                        p.memo.append('Награды за посты в Readdle.me')
-                    elif original_memo in ['', 'award']:
-                        if operation['op'][1]['initiator'] == operation['op'][1]['receiver'] == 'viz-social-bot':
-                            p.memo.append('Самоапы viz-social-bot')
-                        elif operation['op'][1]['initiator'] == operation['op'][1]['receiver']:
-                            p.memo.append('Самоапы')
-                        else:
-                            p.memo.append('Прямые награды')
-                    elif re.match(r'Награда за пост: https://golos.io/@', original_memo) !=None:
-                        p.memo.append('Награды за посты в GOLOS.IO')
-                    elif operation['op'][1]['receiver'] == 'social':
-                        p.memo.append('Награды через Визонатор')
-                    elif operation['op'][1]['initiator'] == 'dice.id':
-                        p.memo.append('Игровой бот dice.id')
-                    else:
-                        p.memo.append('Другое')
-                if operation['op'][0] == 'benefactor_award':
-                    p.block_num.append(operation['block'])
-                    p.operation_type.append(operation['op'][0])
-                    p.shares.append(float(operation['op'][1]['shares'][:-7]))
-                    p.op_in_trx.append(operation['op_in_trx'])
-                    p.timestamp.append(datetime.fromisoformat(operation['timestamp']))
-                    p.trx_id.append(operation['trx_id'])
-                    p.trx_in_block.append(operation['trx_in_block'])
-                    p.virtual_op.append(operation['virtual_op'])
-                    p.initiator.append(operation['op'][1]['initiator'])
-                    p.receiver.append(operation['op'][1]['receiver'])
-                    p.custom_sequence.append(operation['op'][1]['custom_sequence'])
-                    p.trx_id.append(operation['trx_id'])
-                    if operation['op'][1]['initiator'] == 'dice.id':
-                        p.memo.append('Игровой бот dice.id')
-                    else:
-                        p.memo.append('Бенефициарские')
-                if operation['op'][0] == 'witness_reward':
-                    p.block_num.append(operation['block'])
-                    p.operation_type.append(operation['op'][0])
-                    p.shares.append(float(operation['op'][1]['shares'][:-7]))
-                    p.memo.append('Награды делегатам')
-                    p.op_in_trx.append(operation['op_in_trx'])
-                    p.trx_id.append(operation['trx_id'])
-                    p.virtual_op.append(operation['virtual_op'])
-                    p.witness.append(operation['op'][1]['witness'])
-                    p.timestamp.append(datetime.fromisoformat(operation['timestamp']))
-            if interval >= p.scan_delta:
-                break
-        if interval >= p.scan_delta:
-            break
-    del f
-    print('Создание датафрейма, сортировка...')
-    df = pd.DataFrame({'Дата':p.timestamp, 'Shares':p.shares, 'Когорта':p.memo})
-    del p.block_num, p.operation_type, p.shares, p.witness, p.op_in_trx, p.timestamp, p.trx_id, p.trx_in_block, p.virtual_op, p.initiator, p.receiver, p.memo, p.custom_sequence
-    df = df.sort_values('Дата', ascending=True)
-
-    print('Агрегация данных...')
-    df2 = df[['Дата', 'Shares', 'Когорта']]
-    df2['Дата по часу'] = df2['Дата'].dt.to_period('H').dt.to_timestamp('H')
-    temp_df = pd.pivot_table(df2, values='Shares', index=['Дата по часу'], columns=['Когорта'], aggfunc=np.sum, fill_value=0)
-    del df2, df
-
-    print('Сохранение результатов в CSV-файл...')
-    temp_df.to_csv('receive_awards_by_hour-'+str(p.scan_year)+'.csv')
-    del temp_df
+#Задание когорт, по которым будут собираться и делиться данные по наградам
+cohorts = [
+    'Дата',
+    'Бенефициарские',
+    'Другое',
+    'Игровой бот dice.id',
+    'Каналы телеграм',
+    'Награды делегатам',
+    'Награды за посты в Readdle.me',
+    'Награды через Визонатор',
+    'Прямые награды',
+    'Самоапы',
+    'Самоапы viz-social-bot',
+    'Чаты телеграм',
+    'Чаты телеграм (сплит)',
+    'Эирдропы в чатах телеграм',
+    'Номер последнего блока'
+]
